5adda1ad2a213e8fa07167632cfcff57b3f5d99b
l30br <33370057+l30br@users.noreply.github.com>
2018-09-11 09:15:51 -0300

    Add files via upload

--- src/Network/Send/bRO.pm
+++ src/Network/Send/bRO.pm
@@ -23,24 +23,24 @@ sub new {
 		'083C' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
 		'0437' => ['character_move','a3', [qw(coords)]],
 		'035F' => ['sync', 'V', [qw(time)]],
-		'0202' => ['actor_look_at', 'v C', [qw(head body)]],
-		'07E4' => ['item_take', 'a4', [qw(ID)]],
-		'0362' => ['item_drop', 'v2', [qw(index amount)]],
-		'07EC' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'0364' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'095B' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0869' => ['item_take', 'a4', [qw(ID)]],
+		'087A' => ['item_drop', 'v2', [qw(index amount)]],
+		'0950' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'086A' => ['storage_item_remove', 'v V', [qw(index amount)]],
 		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
-		'0368' => ['actor_name_request', 'a4', [qw(ID)]],	
+		'0940' => ['actor_info_request', 'a4', [qw(ID)]],
+		'096A' => ['actor_name_request', 'a4', [qw(ID)]],	
 		'0811' => ['buy_bulk_buyer', 'a4 a4 a*', [qw(buyerID buyingStoreID itemInfo)]], # Buying Store
 		'0817' => ['buy_bulk_closeShop'],
 		'0815' => ['buy_bulk_openShop', 'a4 c a*', [qw(limitZeny result itemInfo)]], # Selling Store
-		'0365' => ['booking_register', 'v8', [qw(level MapID job0 job1 job2 job3 job4 job5)]], # Booking Register
-		'0281' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
-		'022D' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0802' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
-		'023B' => ['friend_request', 'a*', [qw(username)]],
-		'0361' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
-		'0871' => ['storage_password'],		
+		'08A6' => ['booking_register', 'v8', [qw(level MapID job0 job1 job2 job3 job4 job5)]], # Booking Register
+		'0899' => ['item_list_res', 'v V2 a*', [qw(len type action itemInfo)]],
+		'0898' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'0202' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
+		'0835' => ['friend_request', 'a*', [qw(username)]],
+		'0877' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'0960' => ['storage_password'],		
 	);
 	
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
@@ -54,7 +54,7 @@ sub new {
 	
 	while (my ($k, $v) = each %packets) { $handlers{$v->[0]} = $k}
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
-	$self->cryptKeys(1275100544, 1971352960, 1971352960);
+	$self->cryptKeys(102649151, 1748310735, 256208379);
 
 	return $self;
 }
